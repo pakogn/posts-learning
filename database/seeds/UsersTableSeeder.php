627eb0b984312082a2adfe740a59328ee3ef158c
Francisco Daniel Garcia Noriega <pako.gn@hotmail.com>
2019-08-16 09:53:50 -0500

    se anade soporte para factories y seeders.

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Administrador',
+            'email' => 'admin@sistema.com',
+            'password' => bcrypt('admin2019'),
+        ]);
+    }
+}
